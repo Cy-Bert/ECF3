3fe149850a32911b4f67248eca0baf86446f5399
Cy-Bert <cyril.humbert.70@gmail.com>
2023-08-31 16:07:42 +0200

    🐛 corriger(index.html) : ajouter une fenêtre modale pour afficher plus d'informations sur Besançon ✨ fonctionnalité(index.html) : ajouter des animations à la fenêtre modale pour une meilleure expérience utilisateur 🐛 corriger(script.js) : corriger une erreur de syntaxe et ajouter des fonctionnalités pour ouvrir et fermer la fenêtre modale

--- script.js
+++ script.js
@@ -2,3 +2,24 @@ document.querySelector("#heart").onclick = function () {
     document.querySelector("#emptyHeart").classList.toggle("hidden");
     document.querySelector("#fullHeart").classList.toggle("hidden");
 };
+document.querySelector('#buttonMore').onclick = function () {
+    let div = document.querySelector("#closeMore");
+    div.classList.toggle("hidden");
+    div.animate(
+        [
+            //keyframes
+            { transform: 'translateY(100%' },
+            { transform: 'translateY(-5%' }, //Ajout d'un rebont
+            { transform: 'translateY(0' },
+        ],
+        {  //timing options
+            duration: 2000,
+            iterations: 1
+        }
+    )
+
+}
+
+document.querySelector('#closedArrow').onclick = function () {
+    document.querySelector('#closeMore').classList.toggle("hidden");
+}
