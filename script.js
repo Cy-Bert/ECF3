11af5b90df93b88a0776627d673bd8127aa5078f
Cy-Bert <cyril.humbert.70@gmail.com>
2023-08-31 15:55:12 +0200

    heart  js ok

---
+++ script.js
@@ -0,0 +1,4 @@
+document.querySelector("#heart").onclick = function () {
+    document.querySelector("#emptyHeart").classList.toggle("hidden");
+    document.querySelector("#fullHeart").classList.toggle("hidden");
+};
